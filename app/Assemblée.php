795f60fd96148c6a94494a3b282e1ab5003cdec7
Thibaud Robin <Thibaud@client-172-18-91-234.clients.u-psud.fr>
2017-02-07 16:37:31 +0100

    Upload Assemblee

--- app/Assemblée.php
+++ app/Assemblée.php
@@ -9,7 +9,7 @@ class Assemblée extends Model
     public $timestamps = 'false';
 	protected $table = 'Assemblée';
 	protected $primaryKey = 'id_Assemblée';
-	protected $fillable = array('titre', 'dateEv', 'heureDeb', 'heureFin', 'evenementPrivé', 'coutParticipation');
+	protected $fillable = array('Titre', 'dateEv', 'heureDeb', 'heureFin', 'evenementPrivé', 'coutParticipation');
 }
 
 // Ecrit par Thibaud ROBIN - www.thibaud-robin.com
