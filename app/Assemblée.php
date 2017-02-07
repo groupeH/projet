9403f1228e62a282b38bc5b17fcaa0405253dcac
Thibaud Robin <Thibaud@client-172-18-91-234.clients.u-psud.fr>
2017-02-07 16:29:49 +0100

    modification Assemblee

--- app/Assemblée.php
+++ app/Assemblée.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Assemblée extends Model
 {
-    //
+    public $timestamps = 'false';
+	protected $table = 'Assemblée';
+	protected $primaryKey = 'id_Assemblée';
+	protected $fillable = array('titre', 'dateEv', 'heureDeb', 'heureFin', 'evenementPrivé', 'coutParticipation');
 }
-//Thibaud ROBIN
+
+// Ecrit par Thibaud ROBIN - www.thibaud-robin.com
