161c31957d94536a761ba80ab8014c8caddb4e7a
Dany .L <Dany .L>
2017-02-20 12:01:55 +0100

    Début de la vue concernant le formulaire

---
+++ resources/views/deposerdoc.blade.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Déposer des documets</title>
+</head>
+<body>
+<h1>Déposer des documents : </h1>
+<form method="POST" action="C:\wamp\www\projetS4\projet\routes\web.php" enctype="multipart/form-data">	
+     <!-- Le contenu du formulaire est à placer ici... -->
+     <label name="depotdoc" value="Déposer le document"></label>
+     <input type="file" name="fichierdepot">
+     <input type="submit" name="Mettre en ligne" >
+</form>
+</body>
+</html>
+
+
+
+
+<?php
+
+
+
+
+
+
+
+?>
