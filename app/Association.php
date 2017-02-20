6fbcaefec401a5dbf1eaa2a5592aff237bb7b84b
Quentin <quentincettier@MacBook-Pro-de-Quentin.local>
2017-02-20 10:17:09 +0100

    blabla

--- app/Association.php
+++ app/Association.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Association extends Model
 {
 	public $timestamp = 'false';
-    protected $privateKey = "id_Association";
+    protected $primaryKey = "id_Association";
     protected $table = "Association";
     protected $fillable = array('nomAssos', 'mailAssos', 'telephoneAssos', 'description', 'pays', 'info_suplementaire', 'id_Adresse');
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/listeMembre', function (){
+    return view('membre.listeMembre');
+});
