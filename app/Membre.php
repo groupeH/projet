a23c37c7fc71651f554b40692fa81814aadce68e
denilsw1 <denilson7@hotmail.fr>
2017-02-21 17:32:42 +0100

    Correction des modeles Membre et Responsable

--- app/Membre.php
+++ app/Membre.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 
 namespace App;
@@ -13,19 +13,4 @@ class Membre extends Utilisateur
     protected $primaryKey = array('id_Membre','id_Utilisateur');
  
 }
-=======
-<?php
 
-namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Membre extends Utilisateur
-{
-    //
-    public $timestamps = 'false';
-    protected $table = 'Membre';
-    protected $primaryKey = array('id_Membre','id_Utilisateur');
- 
-}
->>>>>>> 662090cc9ec9a219a251f197fa724948787af46f
